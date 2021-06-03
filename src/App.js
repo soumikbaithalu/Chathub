196d7f8d5a268e53fcd178861e1e631c2cae8eb3
soumik2012 <66599363+soumik2012@users.noreply.github.com>
2021-06-03 20:31:39 +0530

    adding the file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    // BEM naming convention 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lets build a WhatsApp Clone!</h1>
+
+      <div className ="app__body">
+      {/* SideBar */}
+      {/* Chat */}
+      </div>
+
+
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBaIYyoUD9Xl_fCaf2Hrvvg04Wm7CEfsN8",
+    authDomain: "whatsapp-clone-react-f7071.firebaseapp.com",
+    projectId: "whatsapp-clone-react-f7071",
+    storageBucket: "whatsapp-clone-react-f7071.appspot.com",
+    messagingSenderId: "495403742208",
+    appId: "1:495403742208:web:e70a9f1dd7a087a063d4e5",
+    measurementId: "G-JWKW7KGYMY"
+  };
