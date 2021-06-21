ac0e1c9adb848744ecd79fe0c97813d053d227dd
Jay Jackson <thebotlynoob@gmail.com>
2021-06-20 22:23:39 -0400

    fix `Chat.js`

--- src/Chat.js
+++ src/Chat.js
@@ -16,7 +16,7 @@ function Chat() {
   const { roomId } = useParams();
   const [roomName, setRoomName] = useState("");
   const [messages, setMessages] = useState([]);
-  const [{ user }, dispatch] = useStateValue();
+  const [{ user }] = useStateValue();
 
   useEffect(() => {
     if (roomId) {
