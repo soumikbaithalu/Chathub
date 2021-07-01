12303a7730b01b25954aa2c372ac35f16d79fda0
Restyled.io <commits@restyled.io>
2021-07-01 23:32:48 +0000

    Restyled by prettier

--- src/Chat.js
+++ src/Chat.js
@@ -1,79 +1,82 @@
-import './Chat.css';
+import "./Chat.css";
 
-import {Avatar, IconButton} from '@material-ui/core';
-import {AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons';
-import ContentCopyIcon from '@material-ui/icons/ContentCopy';
-import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
-import MicIcon from '@material-ui/icons/Mic';
-import firebase from 'firebase';
-import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import ContentCopyIcon from "@material-ui/icons/ContentCopy";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+import firebase from "firebase";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import db from './firebase';
-import {useStateValue} from "./StateProvider";
+import db from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function updateClipboard(newClip) {
   navigator.clipboard.writeText(newClip).then(
-      function() {
-        // alert("Copied the text: " + newClip);
-        document.getElementById("custom-tooltip").style.display = "inline";
-        document.execCommand("copy");
-        setTimeout(function() {
-          document.getElementById("custom-tooltip").style.display = "none";
-        }, 1000);
-      },
-      function() {
-        /* clipboard write failed */
-      });
+    function () {
+      // alert("Copied the text: " + newClip);
+      document.getElementById("custom-tooltip").style.display = "inline";
+      document.execCommand("copy");
+      setTimeout(function () {
+        document.getElementById("custom-tooltip").style.display = "none";
+      }, 1000);
+    },
+    function () {
+      /* clipboard write failed */
+    }
+  );
 }
 
 function Chat() {
   const [input, setInput] = useState("");
   const [seed, setSeed] = useState("");
-  const {roomId} = useParams();
+  const { roomId } = useParams();
   const [roomName, setRoomName] = useState("");
   const [messages, setMessages] = useState([]);
-  const [{user}] = useStateValue();
+  const [{ user }] = useStateValue();
 
   useEffect(() => {
     if (roomId) {
-      db.collection('rooms').doc(roomId).onSnapshot(
-          snapshot => (setRoomName(snapshot.data().name)));
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomName(snapshot.data().name));
 
-      db.collection('rooms')
-          .doc(roomId)
-          .collection("messages")
-          .orderBy("timestamp", "asc")
-          .onSnapshot(snapshot =>
-                          (setMessages(snapshot.docs.map(doc => doc.data()))));
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
     }
-  }, [ roomId ])
+  }, [roomId]);
 
-  useEffect(() => { setSeed(Math.floor(Math.random() * 5000)); }, [ roomId ]);
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, [roomId]);
 
   const sendMessage = (e) => {
     e.preventDefault();
-    db.collection('rooms').doc(roomId).collection('messages').add({
-      message : input,
-      name : user.displayName,
-      timestamp : firebase.firestore.FieldValue.serverTimestamp(),
-    })
+    db.collection("rooms").doc(roomId).collection("messages").add({
+      message: input,
+      name: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
 
     setInput("");
-  }
+  };
 
   return (
-    <div className='chat'>
-      <div className='chat__header'>
-        <Avatar src={
-    `https://avatars.dicebear.com/api/human/${seed}.svg`} />
-        <div className='chat__headerInfo'>
-          <h3 className='chat-room-name'>{roomName}</h3>
-          <p className='chat-room-last-seen'>
-            Last seen {" "}
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3 className="chat-room-name">{roomName}</h3>
+          <p className="chat-room-last-seen">
+            Last seen{" "}
             {new Date(
-              messages[messages.length - 1]
-              ?.timestamp?.toDate()
+              messages[messages.length - 1]?.timestamp?.toDate()
             ).toUTCString()}
           </p>
         </div>
@@ -87,35 +90,45 @@ function Chat() {
           <IconButton>
             <MoreVert />
           </IconButton>
-
         </div>
       </div>
-      <div className='chat__body'>
-        {messages.map(message => (
-          <p className={`chat__message ${message.name === user.displayName && 'chat__receiver'}`}>
+      <div className="chat__body">
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${
+              message.name === user.displayName && "chat__receiver"
+            }`}
+          >
             <span className="chat__name">{message.name}</span>
             {message.message}
-            <span className="chat__timestamp" > {new Date(message.timestamp?.toDate()).toUTCString()}</span>
+            <span className="chat__timestamp">
+              {" "}
+              {new Date(message.timestamp?.toDate()).toUTCString()}
+            </span>
           </p>
-        ))
-}
-</div >
-      <div className='chat__footer'>
+        ))}
+      </div>
+      <div className="chat__footer">
         <InsertEmoticonIcon />
-    <form>
-    <input value = {input} onChange = {(e) => setInput(e.target.value)} type =
-         "text" placeholder = "Type a message" />
-    <button type = "submit" onClick = {sendMessage}>Send a
-        Message</button>
-        </form>< div class = "button-tooltip-container">
-        <ContentCopyIcon onClick={() =>  updateClipboard(input)} />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button type="submit" onClick={sendMessage}>
+            Send a Message
+          </button>
+        </form>
+        <div class="button-tooltip-container">
+          <ContentCopyIcon onClick={() => updateClipboard(input)} />
           <span id="custom-tooltip">copied!</span>
         </div>
         <MicIcon />
       </div>
-
-    </div >
-  )
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
