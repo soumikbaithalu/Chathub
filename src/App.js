b7a83f55175f116efd37aae3ad8620f7395d9623
Restyled.io <commits@restyled.io>
2021-07-05 15:37:28 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import Sidebar from './Sidebar';
+
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import Chat from './Chat';
 import Login from './Login';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useStateValue } from './StateProvider';
-
+import Sidebar from './Sidebar';
+import {useStateValue} from './StateProvider';
 
 function App() {
-  const [{ user }] = useStateValue();
+  const [{user}] = useStateValue();
   const [darkMode, setDarkMode] = useState(false);
-  return (
-    <div className={darkMode ? "dark-mode" : "light-mode"}>
-    <div className="switch-checkbox">
-              <label className="switch">
-                <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+  return (<div className = {darkMode ? "dark-mode" : "light-mode"}>
+          <div className = "switch-checkbox"><label className = "switch">
+          <input type = "checkbox" onChange =
+           {
+             () => setDarkMode(!darkMode)
+           } />
                 <span className="slider round"> </span>
-              </label>
-            </div>
-    <div className="app">
-      {!user ? (
-        <Login />
-      ) : (
-        <div className="app__body">
-          <Router>
-            <Sidebar />
-            <Switch>
-              <Route path="/rooms/:roomId">
-                <Chat />
-              </Route>
+          </label>
+            </div><div className = "app">{
+              !user ? (<Login />)
+                    : (<div className = "app__body"><Router><Sidebar /><Switch>
+                       <Route path = "/rooms/:roomId"><Chat />
+                       </Route>
               <Route path="/">
-                <Chat />
-              </Route>
+                        < Chat /></Route>
             </Switch>
-          </Router>
-        </div>
-      )}
+                       </Router>
+        </div>)}
 
-    </div>
-    </div>
-  );
+          </div>
+    </div>);
 }
 
 export default App;
