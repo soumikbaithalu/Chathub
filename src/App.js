0864960a2009fe3a0287b45a615b77a2e6756807
codingninja11 <akritisheresth@gmail.com>
2021-07-05 21:01:01 +0530

    dark-mode feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import Login from './Login';
@@ -9,7 +9,15 @@ import { useStateValue } from './StateProvider';
 
 function App() {
   const [{ user }] = useStateValue();
+  const [darkMode, setDarkMode] = useState(false);
   return (
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+    <div className="switch-checkbox">
+              <label className="switch">
+                <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+                <span className="slider round"> </span>
+              </label>
+            </div>
     <div className="app">
       {!user ? (
         <Login />
@@ -29,6 +37,7 @@ function App() {
         </div>
       )}
 
+    </div>
     </div>
   );
 }
