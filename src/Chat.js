ed6224cc81db3d96e2d5e082784e3359298edb82
Restyled.io <commits@restyled.io>
2021-07-13 12:06:53 +0000

    Restyled by clang-format

--- src/Chat.js
+++ src/Chat.js
@@ -1,46 +1,42 @@
 import "./Chat.css";
 
-import { Avatar, IconButton, Tooltip } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
-import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import {Avatar, IconButton, Tooltip} from "@material-ui/core";
+import {AttachFile, MoreVert, SearchOutlined} from "@material-ui/icons";
 import AssignmentIcon from "@material-ui/icons/Assignment";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
 import firebase from "firebase";
-import React, { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, {useEffect, useRef, useState} from "react";
+import {useParams} from "react-router-dom";
 
 import db from "./firebase";
-import { useStateValue } from "./StateProvider";
+import {useStateValue} from "./StateProvider";
 
 function Chat() {
   const [input, setInput] = useState("");
   const [seed, setSeed] = useState("");
-  const { roomId } = useParams();
+  const {roomId} = useParams();
   const [roomName, setRoomName] = useState("");
   const [messages, setMessages] = useState([]);
-  const [{ user }] = useStateValue();
+  const [{user}] = useStateValue();
   const chatBodyRef = useRef(null);
   const inputRef = useRef(null);
 
   useEffect(() => {
     if (roomId) {
-      db.collection("rooms")
-        .doc(roomId)
-        .onSnapshot((snapshot) => setRoomName(snapshot.data().name));
+      db.collection("rooms").doc(roomId).onSnapshot(
+          (snapshot) => setRoomName(snapshot.data().name));
 
       db.collection("rooms")
-        .doc(roomId)
-        .collection("messages")
-        .orderBy("timestamp", "asc")
-        .onSnapshot((snapshot) =>
-          setMessages(snapshot.docs.map((doc) => doc.data()))
-        );
+          .doc(roomId)
+          .collection("messages")
+          .orderBy("timestamp", "asc")
+          .onSnapshot((snapshot) =>
+                          setMessages(snapshot.docs.map((doc) => doc.data())));
     }
-  }, [roomId]);
+  }, [ roomId ]);
 
-  useEffect(() => {
-    setSeed(Math.floor(Math.random() * 5000));
-  }, [roomId]);
+  useEffect(() => { setSeed(Math.floor(Math.random() * 5000)); }, [ roomId ]);
 
   useEffect(() => {
     chatBodyRef.current.scrollTop = chatBodyRef.current.scrollHeight;
@@ -49,9 +45,9 @@ function Chat() {
   const sendMessage = (e) => {
     e.preventDefault();
     db.collection("rooms").doc(roomId).collection("messages").add({
-      message: input,
-      name: user.displayName,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message : input,
+      name : user.displayName,
+      timestamp : firebase.firestore.FieldValue.serverTimestamp(),
     });
 
     setInput("");
@@ -66,7 +62,8 @@ function Chat() {
   return (
     <div className="chat">
       <div className="chat__header">
-        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <Avatar src={
+    `https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3 className="chat-room-name">{roomName}</h3>
           <p className="chat-room-last-seen">
@@ -102,16 +99,14 @@ function Chat() {
               {new Date(message.timestamp?.toDate()).toUTCString()}
             </span>
           </p>
-        ))}
-      </div>
+        ))
+}
+</div>
       <div className="chat__footer">
         <InsertEmoticonIcon />
-        <form>
-          <input
-            ref= {inputRef}
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            type="text"
+    <form>< input
+ref = {inputRef} value = {input} onChange = {
+    (e) => setInput(e.target.value)} type = "text"
             placeholder="Type a message"
           />
           <button type="submit" onClick={sendMessage}>
@@ -128,6 +123,6 @@ function Chat() {
       </div>
     </div>
   );
-}
+            }
 
-export default Chat;
+            export default Chat;
