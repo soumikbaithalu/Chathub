b57d0fc8f3d46bf65e74a226a65f3609233e387d
Haripriya Baskaran <haripriya.baskaran@yellow.ai>
2021-12-02 12:48:32 +0530

    Feature: Logout configured

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,36 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import './Sidebar.css';
-import { Avatar, IconButton } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton, Menu, MenuItem } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
-import db from './firebase';
-import { useStateValue } from './StateProvider';
+import db, { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
 
 function Sidebar(props) {
-
   const [rooms, setRooms] = useState([]);
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const [{ user }] = useStateValue();
-
+  const dispatch = useStateValue()[1];
   useEffect(() => {
-
-    const unsubscribe = db.collection('rooms').onSnapshot(snapshot => (
-      setRooms(snapshot.docs.map(doc => (
-        {
+    const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
+      setRooms(
+        snapshot.docs.map((doc) => ({
           id: doc.id,
           data: doc.data(),
-        }
+        }))
       )
-
-      ))
-    ))
+    );
 
     return () => {
       unsubscribe();
-    }
+    };
   }, []);
+
+  const handleClickMore = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (event) => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = (event) => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: null,
+      })
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const open = Boolean(anchorEl);
 
   return (
     <div className="sidebar">
@@ -43,10 +63,19 @@ function Sidebar(props) {
           <IconButton>
             <ChatIcon />
           </IconButton>
-          <IconButton>
+          <IconButton onClick={handleClickMore}>
             <MoreVertIcon />
           </IconButton>
-
+          <Menu
+            anchorEl={anchorEl}
+            keepMounted
+            onClose={handleClose}
+            open={open}
+          >
+            <MenuItem key={"logout"} onClick={handleLogout}>
+              Logout
+            </MenuItem>
+          </Menu>
         </div>
       </div>
       <div className="sidebar__search">
@@ -57,7 +86,7 @@ function Sidebar(props) {
       </div>
       <div className="sidebar__chats">
         <SidebarChat addNewChat />
-        {rooms.map(room => (
+        {rooms.map((room) => (
           <SidebarChat key={room.id} id={room.id} name={room.data.name} />
         ))}
       </div>
