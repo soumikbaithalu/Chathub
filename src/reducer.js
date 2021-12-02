99cb653d1bddbd0fc0e3ee2fc91679734dad5d5d
Restyled.io <commits@restyled.io>
2021-12-02 07:23:49 +0000

    Restyled by prettier

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,23 @@
 export const initialState = {
-  user : null,
+  user: null,
 };
 
 export const actionTypes = {
-  SET_USER : "SET_USER",
-  LOGOUT : "LOGOUT"
+  SET_USER: "SET_USER",
+  LOGOUT: "LOGOUT",
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-  case actionTypes.SET_USER:
-    return {
-      ...state,
-      user : action.user,
-    };
-  case actionTypes.LOGOUT:
-    return { user: null }
-  default:
-    return state;
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        user: action.user,
+      };
+    case actionTypes.LOGOUT:
+      return { user: null };
+    default:
+      return state;
   }
 };
 
