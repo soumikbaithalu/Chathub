ac0cbd87ffe8f5fe0bb57f7b799e91dcaefc98ec
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-04-09 14:26:34 -0400

    Keep user logged in after page reload fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import './App.css';
-import React from 'react';
-import Sidebar from './Sidebar';
-import Chat from './Chat';
-import Login from './Login';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useStateValue } from './StateProvider';
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Login from "./Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function App() {
   const [{ user }] = useStateValue();
+  const [currentUser, setCurrentUser] = useState(user);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div className="app">
-      {!user ? (
-        <Login />
-      ) : (
-        <div className="app__body">
-          <Router>
-            <Sidebar />
-            <Switch>
-              <Route path="/rooms/:roomId">
-                <Chat />
-              </Route>
-              <Route path="/">
-                <Chat />
-              </Route>
-            </Switch>
-          </Router>
+      {loading ? (
+        <div className="app">
+          <div className="centeredOnScreen">loading...</div>
         </div>
+      ) : (
+        <>
+          {!currentUser ? (
+            <Login />
+          ) : (
+            <div className="app__body">
+              <Router>
+                <Sidebar />
+                <Switch>
+                  <Route path="/rooms/:roomId">
+                    <Chat />
+                  </Route>
+                  <Route path="/">
+                    <Chat />
+                  </Route>
+                </Switch>
+              </Router>
+            </div>
+          )}
+        </>
       )}
-
     </div>
   );
 }
